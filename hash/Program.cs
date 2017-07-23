4dfad926e7f09d64fa6bbcac887d5b2d13e0e01c
Ammarik <Ammar-Alyasr@users.noreply.github.com>
2017-07-23 15:15:02 +0300

    remember

--- hash/Program.cs
+++ hash/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Cryptography;
@@ -8,36 +9,78 @@ using System.Threading.Tasks;
 
 namespace hash
 {
+    
     class Program
     {
-
+        
         static void Main(string[] args)
         {
 
-            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-4SEP11A;Initial Catalog=Citizens;Integrated Security=SSPI;MultipleActiveResultSets=True");
-
-
-            baglanti.Open();
             
-       
+            void kapak()
+            {
+                //database class
+                database db = new database();
+
+                SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-4SEP11A;Initial Catalog=Citizens;Integrated Security=SSPI;MultipleActiveResultSets=True");
+                //string tc = "99559453436";
+                //var dr = db.GetDataRow("SELECT *  FROM tbl_citizens where tc_no= '" + tc + "' ");
+                //Console.WriteLine(dr["email"].ToString());
+                ////database class
+
+
+
+
+
+
+                baglanti.Open();
+
+                string TC = "00559453436";
+
+                string resss = "258025802vergtgtgitabani.com";
+                var passs = SecurePasswordHasher.Hash(resss);
+
+                string mail = "buneeradi@com";
+
+                string phone = "5050505";
+
+                DataTable sql5 = db.GetDataTable("select * from tbl_citizens");
+
+
+
+
+                if (sql5.Rows.Count > 0)
+                {
+                    foreach (DataRow dvr in sql5.Rows)
+                    {
+                        Console.Write(Environment.NewLine + dvr["email"].ToString());
+                    }
+                }
+
+
+
+                var sql = db.GetDataSet("INSERT INTO tbl_citizens (tc_no, password, email, phone_number) values  ('" + TC + "' , '" + passs + "', '" + mail + "' ,'" + phone + "' ) ");
+
+            }
+
             void ekle()
             {
                 try
                 {
                
-                    string sifrem = "258025802qaz";
-                    var hasht = SecurePasswordHasher.Hash(sifrem);
-                    string sql = "insert into info(Tc, User_name, pass, email, tel) values (@tc,@isim,@pass,@email,@tel)";
+                    string sifrem = "258frd802qaz";
+                    
+                    string sql2 = "insert into tbl_citizens(tc_no, password, email, phone_number) values (@tc,@pass,@email,@tel)";
 
 
-                    SqlCommand komut = new SqlCommand(sql, baglanti);
-                    komut.Parameters.AddWithValue("@tc", "8888");
-                    komut.Parameters.AddWithValue("@isim", "denemem");
-                    komut.Parameters.AddWithValue("@pass", hasht);
-                    komut.Parameters.AddWithValue("@email", "hashlandi@com");
-                    komut.Parameters.AddWithValue("@tel", "2222");
+                    SqlCommand komut2 = new SqlCommand(sql2, baglanti);
+                    komut2.Parameters.AddWithValue("@tc", "77559453436");
+                  
+                    komut2.Parameters.AddWithValue("@pass", sifrem);
+                    komut2.Parameters.AddWithValue("@email", "hashlandi@com");
+                    komut2.Parameters.AddWithValue("@tel", "2222");
 
-                    komut.ExecuteNonQuery();
+                    komut2.ExecuteNonQuery();
 
 
                     baglanti.Close();
@@ -49,45 +92,41 @@ namespace hash
                 }
             }
 
-
             void dene()
             {
                 string username = "hashlandi@com";
                 string word = "258025802qaz";
 
-                String sql = @"SELECT * FROM [info] WHERE [email] =@email";
-
-
-                
+                String sql = @"SELECT * FROM [tbl_citizen_info] WHERE [citizen_email] =@email";
                 SqlCommand comm = new SqlCommand(sql, baglanti);
                 comm.Parameters.AddWithValue("@email", username);
 
-                
                 SqlDataReader nwReader = comm.ExecuteReader();
+
                 while (nwReader.Read())
                 {
-                    string email = (string)nwReader["email"];
-                    string pass = (string)nwReader["pass"];
+                    string email = (string)nwReader["citizen_email"];
+                    string pass = (string)nwReader["citizen_password"];
 
                     var res = SecurePasswordHasher.Verify(word, pass);
 
-                    Console.WriteLine(res);
+                    //Console.WriteLine(res);
                     if (res == true)
                     {
-                        Console.Write("tamamdir");
+                        Console.Write(Environment.NewLine + "Sifre Dogrudur...");
                     }
 
 
                 }
-                
+
 
                 nwReader.Close();
                 baglanti.Close();
 
-               
+
             }
-            dene();
             
+
             Console.Read();
         }
 
